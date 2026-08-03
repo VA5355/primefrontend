e146fd80adaecff6c8a318bd95e997628da164fa
Vinayak Anvekar <fairvinay@gmail.com>
2026-08-03 11:28:29 +0530

    re-align Admin products same as Shop

--- src/pages/admin/AdminProducts.jsx
+++ src/pages/admin/AdminProducts.jsx
@@ -36,8 +36,49 @@ export default function AdminProducts ()
     try
     {
       const { data } = await axios.get( "/products" );
-      setProducts( data );
-      setFilteredProducts( data );
+       let productsPhotoPathUpdate =    data.map(p => { 
+          if(p !==undefined && p.photoPath !==undefined && p.photoPath !==null){
+         let imgPath = p.photoPath.toString();
+         //product.photoPath && isPhotoCloudinary ?  product.photoPath : (product.photoPath ? `${process.env.REACT_APP_API_PHOTOS}${slash}${product.photoPath}` : '/placeholder.png'
+        if(imgPath.toLowerCase().startsWith("https://res.cloudinary.com")){
+                  //  setPhoto(data.photoPath.toString());
+                    console.log('Cloudinary url available ');
+              console.log(' Cloudinary url  '+imgPath );   
+              
+              p.photoPath = imgPath;
+                 //   setIsPhotoCloudinary(true)
+                    //setIsCreateObject(false)
+          }
+        else {
+
+              let photoFirstChar  =  imgPath.slice(0,1);
+              let isForwardSlash = photoFirstChar==='/' ? true : false;
+            console.log(' photoPath contains forwardslash '+isForwardSlash );
+            if(isForwardSlash)
+            {
+              console.log(' photoPath no forwardslash  required '+isForwardSlash );
+                //  setSlash('');
+             // return process.env.REACT_APP_API_PHOTOS+imgPath;
+                p.photoPath = process.env.REACT_APP_API_PHOTOS+imgPath;
+            }else {
+                console.log(' photoPath  forwardslash  required '+(!isForwardSlash) );
+             //   setSlash('/');
+             //   return process.env.REACT_APP_API_PHOTOS+'/'+imgPath;
+                p.photoPath =  process.env.REACT_APP_API_PHOTOS+'/'+imgPath;
+            }
+          
+        }
+      }
+       else {
+            p.photoPath = 
+              '/placeholder.png'
+        }
+        return p;
+      })
+
+
+      setProducts( productsPhotoPathUpdate );
+      setFilteredProducts( productsPhotoPathUpdate );
     } catch ( error )
     {
       console.log( error );
@@ -186,7 +227,7 @@ export default function AdminProducts ()
                                 <div className="relative h-48 md:h-full overflow-hidden rounded-l-lg">
                                   {/*product?.photoPath ? `${process.env.REACT_APP_API_PHOTOS}${product.photoPath}` : '/placeholder.png' */}
                                   <img
-                                    src={getProductPath()}
+                                    src={product?.photoPath ?  product.photoPath  : '/placeholder.png' }
                                     alt={product.name}
                                     className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-105"
                                   />
